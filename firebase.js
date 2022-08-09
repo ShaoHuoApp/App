cbb31e8342bd14fae9aaa1151dc2148ce344cdf0
XingyuLi20 <lixingyu@berkeley.edu>
2022-08-09 01:42:54 -0700

    add auth

---
+++ firebase.js
@@ -0,0 +1,52 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getStorage, ref as storageRef, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { getDatabase, ref as databaseRef, set as databaseSet, onValue as databaseOnValue, get as databaseGet, child as databaseChild, off as databaseOff, query as databaseQuery, orderByChild, equalTo } from "firebase/database";
+import Config from "react-native-config";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+console.log(Config.API_KEY);
+const firebaseConfig = {
+//   apiKey: Config.API_KEY,
+//   authDomain: Config.AUTH_DOMAIN,
+//   projectId: Config.PROJECT_ID,
+//   storageBucket: Config.STORAGE_BUCKET,
+//   messagingSenderId: Config.MESSAGEING_SENDER_ID,
+//   appId: Config.APP_ID,
+    apiKey: "AIzaSyAuQmI8ARMUKeMgO0Btk4cJtVrx8m0eafw",
+    authDomain: "shaohuo-5a709.firebaseapp.com",
+    projectId: "shaohuo-5a709",
+    storageBucket: "shaohuo-5a709.appspot.com",
+    messagingSenderId: "474631129226",
+    appId: "1:474631129226:web:e339d0512e03a8a37d0573",
+    measurementId: "G-FT5B2JX0C6"
+};
+console.log(firebaseConfig);
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const storage = getStorage(app);
+const database = getDatabase(app);
+
+export {
+  storage,
+  storageRef,
+  uploadBytesResumable,
+  getDownloadURL,
+
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+
+  database,
+  databaseRef,
+  databaseSet,
+  databaseOnValue,
+  databaseGet,
+  databaseChild,
+  databaseOff,
+  databaseQuery,
+  orderByChild,
+  equalTo
+};
