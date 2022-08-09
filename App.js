49bd53d398848488bf023a9fef2da456169f137f
XingyuLi20 <lixingyu@berkeley.edu>
2022-08-09 01:52:32 -0700

    add login

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LandingScreen  from './components/auth/Landing';
 import RegisterScreen from './components/auth/Register'
+import LoginScreen from './components/auth/Login'
 
 const Stack = createStackNavigator();
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
       <Stack.Navigator initialRouteName = "Landing">
         <Stack.Screen name="Landing" component={LandingScreen} options={{headerShown: false}}/>
         <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- components/auth/Login.js
+++ components/auth/Login.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react'
 import { View, Button, TextInput } from 'react-native';
-import { auth, createUserWithEmailAndPassword } from '../../firebase';
+import { auth, signInWithEmailAndPassword } from '../../firebase';
 
-export class Register extends Component {
+export class Login extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
          email: '',
          password: '',
-         name: ''
     }
 
-    this.onSignUp = this.onSignUp.bind(this)
+    this.onSignIn = this.onSignIn.bind(this)
   }
 
-  onSignUp() {
-    const { email, password, name } = this.state;
-    createUserWithEmailAndPassword(auth, email, password)
+  onSignIn() {
+    const { email, password } = this.state;
+    signInWithEmailAndPassword(auth, email, password)
     .then((result) => {
         console.log(result)
     })
@@ -30,10 +29,6 @@ export class Register extends Component {
     return (
       <View>
         <TextInput 
-            placeholder='name'
-            onChangeText={(name) => this.setState({name})}
-        />
-        <TextInput 
             placeholder='email'
             onChangeText={(email) => this.setState({email})}
         />
@@ -44,12 +39,12 @@ export class Register extends Component {
         />
 
         <Button
-            onPress={() => this.onSignUp( )}
-            title="Sign Up"
+            onPress={() => this.onSignIn( )}
+            title="Sign In "
         />
       </View> 
     )
   }
 }
 
-export default Register
+export default Login
