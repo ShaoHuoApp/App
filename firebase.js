1b6924066b1695d9bd03fa4c471f95968c74aff0
Will Jiang <willjiang@Wills-MacBook-Pro-4.local>
2022-08-12 01:54:49 -0700

    update gitignore

--- firebase.js
+++ firebase.js
@@ -1,26 +1,63 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
-import { getStorage, ref as storageRef, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { getDatabase, ref as databaseRef, set as databaseSet, onValue as databaseOnValue, get as databaseGet, child as databaseChild, off as databaseOff, query as databaseQuery, orderByChild, equalTo } from "firebase/database";
-import { getFirestore, collection, doc, setDoc, getDoc, getDocs, addDoc, serverTimestamp, query as firestoreQuery, orderBy } from "firebase/firestore"; 
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import {
+  getStorage,
+  ref as storageRef,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import {
+  getDatabase,
+  ref as databaseRef,
+  set as databaseSet,
+  onValue as databaseOnValue,
+  get as databaseGet,
+  child as databaseChild,
+  off as databaseOff,
+  query as databaseQuery,
+  orderByChild,
+  equalTo,
+} from "firebase/database";
+import {
+  getFirestore,
+  collection,
+  doc,
+  setDoc,
+  getDoc,
+  getDocs,
+  addDoc,
+  serverTimestamp,
+  query as firestoreQuery,
+  orderBy,
+} from "firebase/firestore";
 import Config from "react-native-config";
-
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-//   apiKey: Config.API_KEY,
-//   authDomain: Config.AUTH_DOMAIN,
-//   projectId: Config.PROJECT_ID,
-//   storageBucket: Config.STORAGE_BUCKET,
-//   messagingSenderId: Config.MESSAGEING_SENDER_ID,
-//   appId: Config.APP_ID,
-    apiKey: "AIzaSyAuQmI8ARMUKeMgO0Btk4cJtVrx8m0eafw",
-    authDomain: "shaohuo-5a709.firebaseapp.com",
-    projectId: "shaohuo-5a709",
-    storageBucket: "shaohuo-5a709.appspot.com",
-    messagingSenderId: "474631129226",
-    appId: "1:474631129226:web:e339d0512e03a8a37d0573",
-    measurementId: "G-FT5B2JX0C6"
+  //   apiKey: Config.API_KEY,
+  //   authDomain: Config.AUTH_DOMAIN,
+  //   projectId: Config.PROJECT_ID,
+  //   storageBucket: Config.STORAGE_BUCKET,
+  //   messagingSenderId: Config.MESSAGEING_SENDER_ID,
+  //   appId: Config.APP_ID,
+  // apiKey: "AIzaSyAuQmI8ARMUKeMgO0Btk4cJtVrx8m0eafw",
+  // authDomain: "shaohuo-5a709.firebaseapp.com",
+  // projectId: "shaohuo-5a709",
+  // storageBucket: "shaohuo-5a709.appspot.com",
+  // messagingSenderId: "474631129226",
+  // appId: "1:474631129226:web:e339d0512e03a8a37d0573",
+  // measurementId: "G-FT5B2JX0C6"
+  apiKey: "AIzaSyBWHsmk5XjJWkZSuYQfkP8XB23GlPAVrhg",
+  authDomain: "shaohuo-a285e.firebaseapp.com",
+  projectId: "shaohuo-a285e",
+  storageBucket: "shaohuo-a285e.appspot.com",
+  messagingSenderId: "963602629923",
+  appId: "1:963602629923:web:89cf5ad2df042457837896",
 };
 console.log(firebaseConfig);
 
@@ -35,12 +72,10 @@ export {
   storageRef,
   uploadBytesResumable,
   getDownloadURL,
-
   auth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   onAuthStateChanged,
-
   database,
   databaseRef,
   databaseSet,
@@ -51,7 +86,6 @@ export {
   databaseQuery,
   orderByChild,
   equalTo,
-
   firestore,
   collection,
   doc,
@@ -61,5 +95,5 @@ export {
   addDoc,
   serverTimestamp,
   firestoreQuery,
-  orderBy
+  orderBy,
 };
